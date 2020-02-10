68a20e1da2909e8d85f1c7056e0f81a7b70ca514
Emil Lundkvist <emillundkvist@Emils-MacBook-Air.local>
2020-02-10 16:06:52 +0100

    Added player to map

--- src/game.ts
+++ src/game.ts
@@ -1,21 +1,22 @@
 import 'phaser';
 
-export default class Demo extends Phaser.Scene
-{
-    
-    constructor ()
-    {
-        super('demo');
+let player;
+let cursors;
+
+export default class Sheepherder extends Phaser.Scene {
+
+    constructor() {
+        super('Sheepherder');
     }
 
-    preload ()
-    {
+    preload() {
         this.load.image("tiles", "assets/tilesets/tuxmon-sample-32px-extruded.png");
         this.load.tilemapTiledJSON("map", "assets/tilesets/tuxemon-town.json");
+
+        this.load.atlas("atlas", "assets/atlas/atlas.png", "assets/atlas/atlas.json");
     }
 
-    create ()
-    {
+    create() {
         const map = this.make.tilemap({ key: "map" });
 
         // Parameters are the name you gave the tileset in Tiled and then the key of the tileset image in
@@ -26,21 +27,130 @@ export default class Demo extends Phaser.Scene
         const belowLayer = map.createStaticLayer("Below Player", tileset, 0, 0);
         const worldLayer = map.createStaticLayer("World", tileset, 0, 0);
         const aboveLayer = map.createStaticLayer("Above Player", tileset, 0, 0);
+
+        aboveLayer.setDepth(10);
+
+        // Object layers in Tiled let you embed extra info into a map - like a spawn point or custom
+        // collision shapes. In the tmx file, there's an object layer with a point named "Spawn Point"
+        const spawnPoint = map.findObject("Objects", obj => obj.name === "Spawn Point");
+
+        player = this.physics.add
+            .sprite(spawnPoint.x, spawnPoint.y, "atlas", "misa-front")
+            .setSize(30, 40)
+            .setOffset(0, 24);
+
+        const anims = this.anims;
+        anims.create({
+            key: "misa-left-walk",
+            frames: anims.generateFrameNames("atlas", {
+                prefix: "misa-left-walk.",
+                start: 0,
+                end: 3,
+                zeroPad: 3
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        anims.create({
+            key: "misa-right-walk",
+            frames: anims.generateFrameNames("atlas", {
+                prefix: "misa-right-walk.",
+                start: 0,
+                end: 3,
+                zeroPad: 3
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        anims.create({
+            key: "misa-front-walk",
+            frames: anims.generateFrameNames("atlas", {
+                prefix: "misa-front-walk.",
+                start: 0,
+                end: 3,
+                zeroPad: 3
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        anims.create({
+            key: "misa-back-walk",
+            frames: anims.generateFrameNames("atlas", {
+                prefix: "misa-back-walk.",
+                start: 0,
+                end: 3,
+                zeroPad: 3
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        const camera = this.cameras.main;
+        camera.startFollow(player);
+        camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+        cursors = this.input.keyboard.createCursorKeys();
+    }
+
+    update(time, delta) {
+        const speed = 175;
+        const prevVelocity = player.body.velocity.clone();
+
+        // Stop any previous movement from the last frame
+        player.body.setVelocity(0);
+
+        // Horizontal movement
+        if (cursors.left.isDown) {
+            player.body.setVelocityX(-speed);
+        } else if (cursors.right.isDown) {
+            player.body.setVelocityX(speed);
+        }
+
+        // Vertical movement
+        if (cursors.up.isDown) {
+            player.body.setVelocityY(-speed);
+        } else if (cursors.down.isDown) {
+            player.body.setVelocityY(speed);
+        }
+
+        // Normalize and scale the velocity so that player can't move faster along a diagonal
+        player.body.velocity.normalize().scale(speed);
+
+        // Update the animation last and give left/right animations precedence over up/down animations
+        if (cursors.left.isDown) {
+            player.anims.play("misa-left-walk", true);
+        } else if (cursors.right.isDown) {
+            player.anims.play("misa-right-walk", true);
+        } else if (cursors.up.isDown) {
+            player.anims.play("misa-back-walk", true);
+        } else if (cursors.down.isDown) {
+            player.anims.play("misa-front-walk", true);
+        } else {
+            player.anims.stop();
+
+            // If we were moving, pick and idle frame to use
+            if (prevVelocity.x < 0) player.setTexture("atlas", "misa-left");
+            else if (prevVelocity.x > 0) player.setTexture("atlas", "misa-right");
+            else if (prevVelocity.y < 0) player.setTexture("atlas", "misa-back");
+            else if (prevVelocity.y > 0) player.setTexture("atlas", "misa-front");
+        }
     }
 }
+
+
 
 const config = {
     type: Phaser.AUTO,
     backgroundColor: '#125555',
-    width: 800,
-    height: 600,
+    width: 1024,
+    height: 800,
     physics: {
         default: "arcade",
         arcade: {
-          gravity: { y: 0 } // Top down game, so no gravity
+            gravity: { y: 0 } // Top down game, so no gravity
         }
-      },
-    scene: Demo
+    },
+    scene: Sheepherder
 };
 
 const game = new Phaser.Game(config);
