47228fc0b2d00a598b66d60b3eb48718e7b8556f
Emil Lundkvist <emillundkvist@Emils-MacBook-Air.local>
2020-02-10 20:41:58 +0100

    Added classes, work in progress

---
+++ src/Player.ts
@@ -0,0 +1,60 @@
+export default class Player extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene: Phaser.Scene) {
+        super(scene, 200, 200, "atlas");
+
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+        this.setActive(true);
+        this.setCollideWorldBounds(true);
+
+        this.addAnimations(scene);
+    }
+
+    addAnimations(scene: Phaser.Scene) {
+        const anims = scene.anims;
+        anims.create({
+            key: "misa-left-walk",
+            frames: anims.generateFrameNames("atlas", {
+                prefix: "misa-left-walk.",
+                start: 0,
+                end: 3,
+                zeroPad: 3
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        anims.create({
+            key: "misa-right-walk",
+            frames: anims.generateFrameNames("atlas", {
+                prefix: "misa-right-walk.",
+                start: 0,
+                end: 3,
+                zeroPad: 3
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        anims.create({
+            key: "misa-front-walk",
+            frames: anims.generateFrameNames("atlas", {
+                prefix: "misa-front-walk.",
+                start: 0,
+                end: 3,
+                zeroPad: 3
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        anims.create({
+            key: "misa-back-walk",
+            frames: anims.generateFrameNames("atlas", {
+                prefix: "misa-back-walk.",
+                start: 0,
+                end: 3,
+                zeroPad: 3
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+    }
+}
