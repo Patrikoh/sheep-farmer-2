2d615909c5aa90781e247fbfa47c905ae005059c
Emil Lundkvist <emillundkvist@Emils-MacBook-Air.local>
2020-02-10 15:40:56 +0100

    Added example game map

--- src/game.ts
+++ src/game.ts
@@ -2,6 +2,7 @@ import 'phaser';
 
 export default class Demo extends Phaser.Scene
 {
+    
     constructor ()
     {
         super('demo');
@@ -9,30 +10,22 @@ export default class Demo extends Phaser.Scene
 
     preload ()
     {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
+        this.load.image("tiles", "assets/tilesets/tuxmon-sample-32px-extruded.png");
+        this.load.tilemapTiledJSON("map", "assets/tilesets/tuxemon-town.json");
     }
 
     create ()
     {
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
+        const map = this.make.tilemap({ key: "map" });
 
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
+        // Parameters are the name you gave the tileset in Tiled and then the key of the tileset image in
+        // Phaser's cache (i.e. the name you used in preload)
+        const tileset = map.addTilesetImage("tuxmon-sample-32px-extruded", "tiles");
 
-        this.add.image(400, 300, 'libs');
-
-        const logo = this.add.image(400, 70, 'logo');
-
-        this.tweens.add({
-            targets: logo,
-            y: 350,
-            duration: 1500,
-            ease: 'Sine.inOut',
-            yoyo: true,
-            repeat: -1
-        })
+        // Parameters: layer name (or index) from Tiled, tileset, x, y
+        const belowLayer = map.createStaticLayer("Below Player", tileset, 0, 0);
+        const worldLayer = map.createStaticLayer("World", tileset, 0, 0);
+        const aboveLayer = map.createStaticLayer("Above Player", tileset, 0, 0);
     }
 }
 
@@ -41,6 +34,12 @@ const config = {
     backgroundColor: '#125555',
     width: 800,
     height: 600,
+    physics: {
+        default: "arcade",
+        arcade: {
+          gravity: { y: 0 } // Top down game, so no gravity
+        }
+      },
     scene: Demo
 };
 
