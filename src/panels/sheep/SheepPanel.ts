055a442bc23740290f6c2a1ea0c3f9abd20d2eac
Patrik Olsson <patrik.olsson@axakon.se>
2020-02-20 17:07:22 +0100

    fix bottom and top on sheep panel

--- src/panels/sheep/SheepPanel.ts
+++ src/panels/sheep/SheepPanel.ts
@@ -7,10 +7,21 @@ let planks: Phaser.GameObjects.Group;
 export default class SheepPanel {
 
     constructor(scene: Phaser.Scene, herd: SheepHerd) {
-        planks = new Phaser.GameObjects.Group(scene);
+        let topSegment = new Phaser.GameObjects.Sprite(scene, 64, 0, "panels");
+        topSegment.setTexture("panels", "sheep-panel-plank-top-0");
+        topSegment.setScrollFactor(0);
+        topSegment.setDepth(1000);
+        scene.add.existing(topSegment);
 
+        let bottomSegment = new Phaser.GameObjects.Sprite(scene, 64, 32 * herd.getSheep().length + 32, "panels");
+        bottomSegment.setTexture("panels", "sheep-panel-plank-bottom-0");
+        bottomSegment.setScrollFactor(0);
+        bottomSegment.setDepth(1000);
+        scene.add.existing(bottomSegment);
+
+        planks = new Phaser.GameObjects.Group(scene);
         herd.getSheep().forEach((sheep: Sheep, i) => {
-            let plank = new SheepPanelPlank(scene, 64, 32 * i + 16, Type.middle);
+            let plank = new SheepPanelPlank(scene, 64, 32 * i + 32, Type.middle);
             plank.setDepth(1000);
             scene.add.existing(plank);
             plank.setScrollFactor(0);
@@ -19,7 +30,7 @@ export default class SheepPanel {
     }
 
     update(herd: SheepHerd) {
-        planks.getChildren().forEach((plank: SheepPanelPlank, i) =>  plank.update(herd.getSheep().filter(s => s.active)[i]));
+        planks.getChildren().forEach((plank: SheepPanelPlank, i) =>  plank.update(herd.getSheep()[i]));
     }
 
 }
