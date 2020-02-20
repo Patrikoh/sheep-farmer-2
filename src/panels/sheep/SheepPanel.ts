d45157faf612ec3c13eaf25b62fbc197cdfe9f06
Patrik Olsson <patrik.olsson@axakon.se>
2020-02-20 12:59:06 +0100

    fixing sheep panel

--- src/panels/sheep/SheepPanel.ts
+++ src/panels/sheep/SheepPanel.ts
@@ -10,19 +10,12 @@ export default class SheepPanel {
         planks = new Phaser.GameObjects.Group(scene);
 
         herd.getSheep().forEach((sheep: Sheep, i) => {
-            let type: Type = Type.middle;
-            if (i == 0) {
-                type = Type.top;
-            } else if (i === herd.getSheep().length - 1) {
-                type = Type.bottom;
-            }
-            let plank = new SheepPanelPlank(scene, 64, 32 * i + 16, type);
+            let plank = new SheepPanelPlank(scene, 64, 32 * i + 16, Type.middle);
             plank.setDepth(1000);
             scene.add.existing(plank);
             plank.setScrollFactor(0);
             planks.add(plank);
         });
-
     }
 
     update(herd: SheepHerd) {
