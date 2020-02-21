6217451df5ed33312e22a87aa7a207bb9cb9c8a3
Emil Lundkvist <emil@lundkvist.xyz>
2020-02-21 16:27:40 +0100

    Changed port to 5000

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -64,7 +64,7 @@ export default {
             open: true,
             contentBase: 'dist',
             host: 'localhost',
-            port: 3000,
+            port: 5000,
             headers: {
                 'Access-Control-Allow-Origin': '*'
             }
