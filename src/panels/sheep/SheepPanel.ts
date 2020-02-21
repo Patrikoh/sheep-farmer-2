25921bf2c6398561b1751c7975b0f392f60729cd
Patrik Olsson <patrik.olsson@axakon.se>
2020-02-21 11:54:15 +0100

    fix text and lifebar alignement

--- src/panels/sheep/SheepPanel.ts
+++ src/panels/sheep/SheepPanel.ts
@@ -1,6 +1,6 @@
 import SheepHerd from "../../SheepHerd";
 import Sheep from "../../Sheep";
-import SheepPanelPlank, { Type } from "./SheepPanelPlank";
+import SheepPanelPlank from "./SheepPanelPlank";
 
 let planks: Phaser.GameObjects.Group;
 
@@ -8,20 +8,20 @@ export default class SheepPanel {
 
     constructor(scene: Phaser.Scene, herd: SheepHerd) {
         let topSegment = new Phaser.GameObjects.Sprite(scene, 64, 0, "panels");
-        topSegment.setTexture("panels", "sheep-panel-plank-top-0");
+        topSegment.setTexture("panels", "sheep-panel-top-0");
         topSegment.setScrollFactor(0);
         topSegment.setDepth(1000);
         scene.add.existing(topSegment);
 
         let bottomSegment = new Phaser.GameObjects.Sprite(scene, 64, 32 * herd.getSheep().length + 32, "panels");
-        bottomSegment.setTexture("panels", "sheep-panel-plank-bottom-0");
+        bottomSegment.setTexture("panels", "sheep-panel-bottom-0");
         bottomSegment.setScrollFactor(0);
         bottomSegment.setDepth(1000);
         scene.add.existing(bottomSegment);
 
         planks = new Phaser.GameObjects.Group(scene);
         herd.getSheep().forEach((sheep: Sheep, i) => {
-            let plank = new SheepPanelPlank(scene, 64, 32 * i + 32, Type.middle);
+            let plank = new SheepPanelPlank(scene, 64, 32 * i + 32);
             plank.setDepth(1000);
             scene.add.existing(plank);
             plank.setScrollFactor(0);
@@ -30,7 +30,12 @@ export default class SheepPanel {
     }
 
     update(herd: SheepHerd) {
-        planks.getChildren().forEach((plank: SheepPanelPlank, i) =>  plank.update(herd.getSheep()[i]));
+        planks.getChildren().forEach((plank: SheepPanelPlank, i) =>  {
+            const sheep = herd.getSheep()[i];
+            if(sheep) {
+                plank.update(herd.getSheep()[i])
+            }
+        });
     }
 
 }
